5d0c87f761659a65f151138cb4a2059df68fd6a0
vasiliyryzhkovskiy <september86@mail.ru>
2021-02-01 22:59:47 +0300

    add new TestMusicBean class

---
+++ src/test/java/TestMusicBean.java
@@ -0,0 +1,25 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.vasiliyryzhkovskiy.springcourse.MusicPlayer;
+
+/**
+ *
+ * С использованием spring bean - musicPlayerBean
+ * создаём экземляр объекта MusicPlayer
+ *
+ * @author Vasiliy Ryzhkovskiy
+ */
+public class TestMusicBean {
+
+    public static void main(String[] args) {
+        // создаём переменную spring контекста, которая читает файл applicationContext.xml
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml" );
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayerBean", MusicPlayer.class);
+
+        musicPlayer.playMusic();
+
+        // обязательно закрываем контекст
+        context.close();
+    }
+
+}
