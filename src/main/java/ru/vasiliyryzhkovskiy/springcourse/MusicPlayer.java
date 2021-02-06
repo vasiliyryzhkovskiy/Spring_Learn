44578b94dc646b6e7ae58050af58cadeeba9d1b8
vasiliyryzhkovskiy <september86@mail.ru>
2021-02-06 13:59:11 +0300

    add setter to field music in MusicPlayer.java

--- src/main/java/ru/vasiliyryzhkovskiy/springcourse/MusicPlayer.java
+++ src/main/java/ru/vasiliyryzhkovskiy/springcourse/MusicPlayer.java
@@ -6,9 +6,20 @@ package ru.vasiliyryzhkovskiy.springcourse;
 public class MusicPlayer {
     private Music music;
 
-    //IoC
+    /** def constructor need for spring bean musicPlayerSetterBean
+     * @see  /src/main/resources/applicationContext.xml
+     */
+    public MusicPlayer() { }
+
+    // для IoC
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    // для возможности использования spring данного setter
+    public void setMusic(Music music) {
+        this.music = music;
+        System.out.println("yes yes it is  " + MusicPlayer.class.getSimpleName()) ;
     }
 
     public void playMusic() {
